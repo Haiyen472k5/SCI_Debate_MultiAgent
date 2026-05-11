a71460bee3aef300d33ec3b3688b3c2c17496071
Haiyen472k5 <haiyen472k5@gmail.com>
2026-05-11 14:16:58 +0700

    cấu trúc dự án

---
+++ scidebate/app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title('Hello World')
+st.write('Welcome to Streamlit! This is a simple app to demonstrate how to use Streamlit for building interactive web applications.')
